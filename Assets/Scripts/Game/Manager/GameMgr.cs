a2f24afbcaf3097c6daf81be6b792dce3da28270
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-06-25 11:43:24 +0100

    ReadyForExcel

---
+++ Assets/Scripts/Game/Manager/GameMgr.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoSingleton<GameMgr>
+{
+    //private DialogueExcelData dialogueData;
+
+    public override void Init()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Game/PlayModel/BattleUnitBasic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUnitBasic : MonoBehaviour
+{
+    //The current HP of this unit
+    protected float curHP;
+    //The maximum HP of this unit
+    protected float maxHP;
+
+    protected float physATK;
+    protected float magicATK;
+    protected float physDEF;
+    protected float magicDEF;
+}
