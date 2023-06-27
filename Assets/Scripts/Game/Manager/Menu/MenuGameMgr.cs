366a0c00655016681a3de3d30c165ba4e91c20b1
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-06-27 15:01:19 +0100

    Menu

---
+++ Assets/Scripts/Game/Manager/Menu/MenuGameMgr.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGameMgr : MonoBehaviour
+{
+    public MenuUIMgr menuUIMgr;
+
+    private void Start()
+    {
+        
+    }
+}
