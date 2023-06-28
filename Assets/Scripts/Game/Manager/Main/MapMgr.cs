681cb7576f3f787a111e33d1b976f19d4710cc6d
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-06-28 15:11:34 +0100

    Save

--- Assets/Scripts/Game/Manager/Main/MapMgr.cs
+++ Assets/Scripts/Game/Manager/Main/MapMgr.cs
@@ -10,12 +10,21 @@ public class MapMgr : MonoBehaviour
 
     public void Init()
     {
-        for(int i = 0; i < 5; i++)
+        GenerateTile(5, 5);
+    }
+
+    public void GenerateTile(int sizeX,int sizeZ)
+    {
+        int centerX = (sizeX - 1) / 2;
+        int centerZ = (sizeZ - 1) / 2;
+
+
+        for (int i = 0; i < sizeX; i++)
         {
-            for(int j = 0; j < 5; j++)
+            for (int j = 0; j < sizeZ; j++)
             {
-                GameObject objMapTile = GameObject.Instantiate(pfMapTile,new Vector3(i,0,j),Quaternion.identity, tfMapTile);
-                
+                GameObject objMapTile = GameObject.Instantiate(pfMapTile, new Vector3(i - centerX, 0, j - centerZ), Quaternion.identity, tfMapTile);
+                objMapTile.name = string.Format("MapTile{0}_{1}", i, j);
             }
         }
     }
