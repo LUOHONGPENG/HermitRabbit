301c91433e5a75d625df814778c240d83537a9bd
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-10-13 16:57:18 +0100

    NodeFill

--- Assets/Scripts/Game/UI/PageUI/SkillNodeUIItem.cs
+++ Assets/Scripts/Game/UI/PageUI/SkillNodeUIItem.cs
@@ -8,6 +8,7 @@ public class SkillNodeUIItem : MonoBehaviour
 {
     public Image imgFrame;
     public Image imgIcon;
+    public Image imgButton;
 
     public Button btnNode;
 
@@ -17,21 +18,24 @@ public class SkillNodeUIItem : MonoBehaviour
     {
         this.nodeItem = nodeItem;
 
+        //Set Sprite
         if(nodeItem.nodeType == SkillNodeType.Active)
         {
             imgFrame.sprite = Resources.Load("Sprite/Skill/imgIconSkill_Active", typeof(Sprite)) as Sprite;
+            imgButton.sprite = Resources.Load("Sprite/Skill/imgIconSkill_ActiveFill", typeof(Sprite)) as Sprite;
         }
         else if(nodeItem.nodeType == SkillNodeType.Passive)
         {
             imgFrame.sprite = Resources.Load("Sprite/Skill/imgIconSkill_Passive", typeof(Sprite)) as Sprite;
+            imgButton.sprite = Resources.Load("Sprite/Skill/imgIconSkill_PassiveFill", typeof(Sprite)) as Sprite;
         }
         imgIcon.sprite = Resources.Load("Sprite/Skill/" + nodeItem.iconUrl, typeof(Sprite)) as Sprite;
 
-/*        btnNode.onClick.RemoveAllListeners();
+        btnNode.onClick.RemoveAllListeners();
         btnNode.onClick.AddListener(delegate ()
         {
-
-        });*/
+            //Check Whether the node is unlocked
+        });
     }
 
 }
