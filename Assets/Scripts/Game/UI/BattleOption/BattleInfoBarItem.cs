4e8668d48adb13eaacae015c20892fb41260f935
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-11-14 15:28:10 +0000

    Resource

--- Assets/Scripts/Game/UI/BattleOption/BattleInfoBarItem.cs
+++ Assets/Scripts/Game/UI/BattleOption/BattleInfoBarItem.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class BattleInfoBarItem : MonoBehaviour
 {
     public Image imgFill;
+    public Image imgIcon;
     public Text codeInfo;
+    public List<Sprite> listIcon = new List<Sprite>();
     public List<Color> listColor = new List<Color>();
     public List<Color> listColorText = new List<Color>();
 
@@ -18,18 +20,22 @@ public class BattleInfoBarItem : MonoBehaviour
         switch (resourceType)
         {
             case BarResourceType.Health:
+                imgIcon.sprite = listIcon[0];
                 imgFill.color = listColor[0];
                 codeInfo.color = listColorText[0];
                 break;
             case BarResourceType.Skill:
+                imgIcon.sprite = listIcon[1];
                 imgFill.color = listColor[1];
                 codeInfo.color = listColorText[1];
                 break;
             case BarResourceType.Move:
+                imgIcon.sprite = listIcon[2];
                 imgFill.color = listColor[2];
                 codeInfo.color = listColorText[2];
                 break;
         }
+        imgIcon.SetNativeSize();
     }
 
     public void UpdateData(float cur, float max)
